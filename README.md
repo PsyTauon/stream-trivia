cad78821d14bfa616af85143612b3eb9e6d633cd
Psy Tauon <psytauon@gmail.com>
2021-01-13 15:03:19 -0700

    MVP go api

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Stream Trivia
 This is the monorepo for the Stream Trivia game!
+
+## Running the Go API
+Any time any changes are made to the Go source files you need to recompile the application. Use the following command to do so:
+```
+docker-compose down && docker-compose up -d --build
+```
---
+++ go-api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "log"
+
+    "github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+}
+
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Index)
+
+    log.Fatal(http.ListenAndServe(":80", router))
+}
