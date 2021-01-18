ad8c316a651b830b8c4df618033e8f6c1f7e99f8
Psy Tauon <psytauon@gmail.com>
2021-01-18 01:08:48 -0700

    Added flexbox, css grid, and user answers

---
+++ ui/src/components/UserAnswer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="border rounded-lg h-32 flex flex-col px-4">
+        <div class="flex-1">
+            {{ username }}
+        </div>
+        <div class="flex-grow">
+            {{ answer }}
+        </div>
+        <div class="flex-1 flex">
+            <i class="full flex-1">❌</i>
+            <i class="full flex-1">✔️</i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['username', 'answer']
+}
+</script>
--- ui/src/views/Game.vue
+++ ui/src/views/Game.vue
@@ -1,29 +1,38 @@
 <template>
-  <div>
-    <br />
-    <br />
-    <label>{{ gameState }}</label>
-    <br />
-    <br />
-    {{ countDown }}
-    <br />
-    <br />
-    <textarea class="input" v-model="questionText" placeholder="Question" />
-    <br />
-    <br />
-    <textarea class="input" v-model="answer" placeholder="Answer" />
-    <br />
-    <br />
-    <Button class="button" @click="SendQuestion()" title="Add Question" />
+  <div class="flex flex-col md:grid md:grid-cols-2 md:gap-4">
+    <div class="order-3 md:order-none md:col-span-2">
+      {{ countDown }}
+    </div>
+    <div class="order-1 md:order-none">
+      <div>
+        <div class="flex flex-col space-y-4 items-center mb-4">
+          <textarea class="input max-w-xl" v-model="questionText" placeholder="Question" />
+          <textarea class="input max-w-xl" v-model="answer" placeholder="Answer" />
+        </div>
+        <Button class="button" @click="SendQuestion()" title="Add Question" />
+      </div>
+    </div>
+    <div class="order-4 md:order-none md:flex gap-2">
+      <UserAnswer username="Psy" answer="This is an answer" />
+      <UserAnswer username="birdwizard_" answer="Another answer" />
+    </div>
+    <div class="order-3 md:order-none">
+      Player list
+    </div>
+    <div class="order-5 md:order-none">
+      <label>{{ gameState }}</label>
+    </div>
   </div>
 </template>
 
 <script>
 import Button from '../components/Button'
+import UserAnswer from '../components/UserAnswer'
 export default {
   name: "Game",
   components: {
-    Button
+    Button,
+    UserAnswer
   },
   data: () => ({
     questionText: "",
